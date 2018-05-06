59fe5c702a38d1db7fd78e9fe08eefec88bb6b4e
Gerardo Junior <gerardo.barbosa.da.silva.jr@gmail.com>
2018-05-06 16:57:31 -0300

    Added vendor/bin in $PATH

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 if [ -e "$(pwd)/composer.json" ]; then
-    /usr/local/bin/php /usr/local/bin/composer -vvv --no-interaction --prefer-source
+    /usr/local/bin/php /usr/local/bin/composer install -vvv --no-interaction
+fi
+
+if [ -d "$(pwd)/vendor/bin" ]; then
+    export PATH=$PATH:$(pwd)/vendor/bin
 fi
 
 if [ ! -z $@ ]; then
--- tools/start.sh
+++ tools/start.sh
@@ -3,7 +3,11 @@
 curl -L https://api.github.com/repos/gerardo-junior/TAP.api/tarball/master | tar xz --strip-components=1 --directory=$(pwd)
 
 if [ -e "$(pwd)/composer.json" ]; then
-    /usr/local/bin/php /usr/local/bin/composer -vvv --no-interaction --prefer-source
+    /usr/local/bin/php /usr/local/bin/composer install -vvv --no-interaction
+fi
+
+if [ -d "$(pwd)/vendor/bin" ]; then
+    export PATH=$PATH:$(pwd)/vendor/bin
 fi
 
 if [ ! -z $@ ]; then
